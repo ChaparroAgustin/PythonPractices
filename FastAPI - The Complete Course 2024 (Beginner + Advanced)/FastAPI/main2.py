d9f3169d062e28ad4c7a63a3f5f10272b6b99bcd
ChaparroAgustin <chaparroagustin91@hotmail.com>
2024-03-15 16:59:23 -0300

    Practice

--- FastAPI - The Complete Course 2024 (Beginner + Advanced)/FastAPI/main2.py
+++ FastAPI - The Complete Course 2024 (Beginner + Advanced)/FastAPI/main2.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from datetime import date as dt
 
 app = FastAPI()
 
@@ -11,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    publishDate: int
 
-    def __init__(self,id, title, author,description,rating):
+    def __init__(self,id, title, author,description,rating,publishDate):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publishDate = publishDate
 
 class BookRequest(BaseModel):
     id: Optional[int] = None
@@ -25,6 +28,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6) #gt= grater than and lt= lower than 
+    publishDate: int = Field(lt = dt.today().year + 1)
 
     class Config:
         json_schema_extra = {
@@ -32,18 +36,19 @@ class BookRequest(BaseModel):
                 'title': 'A new book',
                 'author': 'Coding on python',
                 'description': 'A new description',
-                'rating': 3
+                'rating': 3,
+                'pubish_date': 2012
             }
         }
 
 
 BOOKS = [
-    Book(1, "Computer Science Pro ", "Coding", "A very nice book!", 5),
-    Book(2, "Be Fast with FastAPI ", "Coding", "A great book!", 5),
-    Book(3, "Master Endpoints", "Coding", "A awesome book!", 5),
-    Book(4, "HP1", "Author 1", "Book Description", 5),
-    Book(5, "HP2", "Author 2", "Book Description", 5),
-    Book(6, "HP3", "Author 3", "Book Description", 5),
+    Book(1, "Computer Science Pro ", "Coding", "A very nice book!", 5, 2020),
+    Book(2, "Be Fast with FastAPI ", "Coding", "A great book!", 4, 2015),
+    Book(3, "Master Endpoints", "Coding", "A awesome book!", 2, 2003),
+    Book(4, "HP1", "Author 1", "Book Description", 3, 2017),
+    Book(5, "HP2", "Author 2", "Book Description", 1, 2019),
+    Book(6, "HP3", "Author 3", "Book Description", 5, 2011),
 ]
 
 @app.get("/books")
@@ -61,22 +66,54 @@ def findeBookId(book: Book):
         book.id = BOOKS[-1].id + 1
     else:
         book.id = 1
-    
     return book
 
 
+@app.get("/book/{id}")
+async def bookById(id: int):
+    for book in BOOKS:
+        if book.id == id:
+            return book
+    else:
+        return {"book not found"}
+
+
+@app.get("/books/")
+async def booksByRating(bookRating: int):
+    if (bookRating < 0 or bookRating > 5):
+        return{"rating must be lower than 6 and higher than 0 "}
+    booksToReturn = []
+    for book in BOOKS:
+        if book.rating == bookRating:
+            booksToReturn.append(book)
+    
+    return booksToReturn
+
+@app.put("/books/update_book")
+async def updateBook(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            return book
+    else:
+        return {"book not found"}
+
+@app.delete("/books/{id}")
+async def deleteBook(bookId : int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == bookId:
+            BOOKS.pop(i)
+            return BOOKS[i]
+        
+    return {"Book not found"}
 
 
 
+@app.get("/books/{date}")
+async def booksBypublish(date: int):
+    booksToReturn = []
+    for book in BOOKS:
+        if book.publishDate == date:
+            booksToReturn.append(book)
 
-
-
-
-
-
-
-
-
-
-
-
+    return booksToReturn
--- FastAPI - The Complete Course 2024 (Beginner + Advanced)/FastAPI/venv/Scripts/activate.bat
+++ FastAPI - The Complete Course 2024 (Beginner + Advanced)/FastAPI/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\achaparro\Documents\Python\FastAPI - The Complete Course 2024 (Beginner + Advanced)\FastAPI\venv
+set VIRTUAL_ENV=G:\Practicas\PythonPractices\FastAPI - The Complete Course 2024 (Beginner + Advanced)\FastAPI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
